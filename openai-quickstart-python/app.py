6cb2105faaaa77bcf0de2712c3d86f1da100ecaa
Haiwei Nan <shonen649@outlook.com>
2023-03-30 00:19:09 -0400

    updated to GPT-4

--- openai-quickstart-python/app.py
+++ openai-quickstart-python/app.py
@@ -4,32 +4,39 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+openai.organization=os.getenv("ORGANIZATION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
+openai.Model.list()
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+        question = request.form["question"]
+        # question=question.format(question.capitalize())
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content":  question},
+        #{{variable}} 是flask的语法，用来标记变量
+            ]
+            )
+        
+        return redirect(url_for("index", result=response['choices'][0]['message']['content']))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
+#example prompt
+# def generate_prompt(animal):
+    
+#     return """Suggest three names for an animal that is a superhero.
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+# Animal: Cat
+# Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+# Animal: Dog
+# Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+# Animal: {}
+# Names:""".format(
+#         animal.capitalize()
+#     )
